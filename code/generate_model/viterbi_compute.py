412a57d2ff8676854208efed0d10abbfba7e913d
Andrea Guzzo <andrea.guzzo92@gmail.com>
2018-07-12 18:40:54 +0200

    Viterbi File Correction
    Add a txt file parsing and correction with Viterbi algorithm

--- code/generate_model/viterbi_compute.py
+++ code/generate_model/viterbi_compute.py
@@ -1,5 +1,6 @@
+import os
 
-
+#Viterbi prompt single word check
 def viterbi_check(model,repeat=True):
     while(repeat):
         try_word = input("\nViterbi correction check for test \nWrite 0 if you want to exit\nPlease enter a word to test the correction: ")
@@ -13,3 +14,30 @@ def viterbi_check(model,repeat=True):
             print("Error validating the input string\nPlease insert only characters\nPlease retry\n")
     
     return 0
+
+#Word correction function for the GUI call
+def word_correction(model,word):
+    result = model.viterbi(word)
+    return result
+
+
+#File correction function to correct a txt file
+def file_correction(model,input_file):
+    result = ""
+    text = ""
+    
+    try:
+        file = open(input_file, 'rt',encoding='UTF-8')
+        text = file.read()
+        file.close()
+    except FileNotFoundError:
+        print("File not found, please insert a valid one")
+
+    for i in text:
+        result = result + " " + model.viterbi(i)
+
+    #Remove the first empty space
+    result.strip()
+
+    print("File corrected")
+    return result
